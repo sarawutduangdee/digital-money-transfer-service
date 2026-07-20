1e15b72567534c65bc9b31f5fc8c759a45a80ffb
Sarawut Duangdee <sarawutduangdee@gmail.com>
2026-07-20 12:33:09 +0700

    add first project

---
+++ src/main/java/com/bank/digital_money_transfer_service/DigitalMoneyTransferServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bank.digital_money_transfer_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalMoneyTransferServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalMoneyTransferServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bank/digital_money_transfer_service/DigitalMoneyTransferServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.digital_money_transfer_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalMoneyTransferServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
